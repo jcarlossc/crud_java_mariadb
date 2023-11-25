b16cd13680e6b58acf849d25908c27a36af0ddb3
Carlos da Costa <129618019+jcarlossc@users.noreply.github.com>
2023-11-25 20:47:00 -0300

    Update MavenSqlJdbc.java

--- src/main/java/com/jcsc/mavensqljdbc/MavenSqlJdbc.java
+++ src/main/java/com/jcsc/mavensqljdbc/MavenSqlJdbc.java
@@ -15,28 +15,28 @@ public class MavenSqlJdbc {
         CustomerController customerController = new CustomerController();
   
         // Insere dados no banco de dados
-        //CustomerModel customerModel = new CustomerModel();
-        //customerModel.setName("Ewellyn Ribeiro");
-        //customerModel.setPhone("81999998888");
-        //customerController.save(customerModel);
+        // CustomerModel customerModel = new CustomerModel();
+        // customerModel.setName("Ewellyn Ribeiro");
+        // customerModel.setPhone("81999998888");
+        // customerController.save(customerModel);
 
         // Atualiza os dados do banco de dados
-        //CustomerModel customerModel = new CustomerModel();
-        //customerModel.setId(26);
-        //customerModel.setName("Teresa Ribeiro");
-        //customerModel.setPhone("81895624587");
-        //customerController.update(customerModel);
+        // CustomerModel customerModel = new CustomerModel();
+        // customerModel.setId(26);
+        // customerModel.setName("Teresa Ribeiro");
+        // customerModel.setPhone("81895624587");
+        // customerController.update(customerModel);
         
 
         // Lista os clientess do banco de dados
-        //List<CustomerModel> customers = customerController.getAll(); 
-        //for(int i = 0; i < customers.size(); i++){
+        // List<CustomerModel> customers = customerController.getAll(); 
+        // for(int i = 0; i < customers.size(); i++){
         //    System.out.println(customers.get(i));
         //    System.out.println("");
-        //}
+        // }
 
         // Apaga dados do banco de dados
-        //customerController.delete(6);
+        // customerController.delete(6);
         
         // Lista a quantidade de clientes
         List<CustomerModel> customers = customerController.getAll();
